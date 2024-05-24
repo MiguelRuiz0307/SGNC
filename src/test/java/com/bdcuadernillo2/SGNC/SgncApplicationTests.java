978ffe26c7b355c9a1c92b2be03dbaa402fd8531
Lenovo <42100559@uaz.edu.mx>
2024-05-24 11:36:31 -0600

    Primer Commit

---
+++ src/test/java/com/bdcuadernillo2/SGNC/SgncApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bdcuadernillo2.SGNC;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SgncApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
